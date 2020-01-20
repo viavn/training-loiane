a87ae234450663189bef12281475af724c6137a6
Vinicius Avansini <vinicius.avansini@writesys.com.br>
2020-01-20 14:44:15 -0300

    #05: Módulos

--- projects/FirstProject/src/app/app.module.ts
+++ projects/FirstProject/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CursosModule } from './cursos/cursos.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CursosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
