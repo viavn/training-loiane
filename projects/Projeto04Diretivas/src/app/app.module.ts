80b71af4ea8501dae7229e1734623b510f8144ba
Vinicius Avansini <vinicius.avansini@writesys.com.br>
2020-01-24 09:01:15 -0300

    #18: Angular CLI: Criando components, services: ng generate

--- projects/Projeto04Diretivas/src/app/app.module.ts
+++ projects/Projeto04Diretivas/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DiretivaIfComponent } from './diretiva-if/diretiva-if.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DiretivaIfComponent
   ],
   imports: [
     BrowserModule,
---
+++ projects/Projeto04Diretivas/src/app/diretiva-if/diretiva-if.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-diretiva-if',
+  templateUrl: './diretiva-if.component.html',
+  styleUrls: ['./diretiva-if.component.css']
+})
+export class DiretivaIfComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
